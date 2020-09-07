f7f67ee5a878a7d0611ebb992a2a08c5f169bde1
David Boone <dboone@jsi.local>
2020-09-07 10:56:31 -0700

    add new branch

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,19 +1,16 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map.
-//  Add a marker to the map for Los Angeles, California.
-// An array containing each city's location, state, and population.
 // Get data from cities.js
 let cityData = cities;
+console.log(cityData);
 
   // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.marker(city.location).addTo(map);
+cityData.forEach(function(cities) {
+	// console.log(city)
+	L.marker(cities.location).addTo(map);
    });
 // We create the tile layer that will be the background of our map.	
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
